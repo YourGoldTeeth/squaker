f1e00872f71a43d21c356fc91e56a2b0cae104e8
James West <jameseswest@gmail.com>
2017-10-04 10:16:40 -0500

    Add validations to user and tweet models

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
 	belongs_to :user
+
+	validates :message, presence: true, length: { maximum: 140, too_long: "A tweet can only be 140 characters long."}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
 
   has_many :tweets
 
+  validates :username, presence: true
+
   mount_uploader :avatar, AvatarUploader
 end
