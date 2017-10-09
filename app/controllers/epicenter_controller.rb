836e04149787f84e94609c71213ffb8d8aad06d1
James West <jameseswest@gmail.com>
2017-10-09 12:02:33 -0500

    add followers, following and feed

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -8,7 +8,7 @@ class EpicenterController < ApplicationController
   def feed
   	@following_tweets = []
 
-  	Tweet.all.each do |tweet|
+  	Tweet.all.order(created_at: :desc).each do |tweet|
   		if current_user.following.include?(tweet.user_id) || current_user.id == tweet.user_id
 
   			@following_tweets.push(tweet)
@@ -16,6 +16,10 @@ class EpicenterController < ApplicationController
   		end	
   	end
   end
+
+  def all_users
+    @users = User.all
+  end  
 
   def show_user
   	@user = User.find(params[:id])
@@ -26,13 +30,36 @@ class EpicenterController < ApplicationController
   	current_user.save
 
   	redirect_to show_user_path(id: params[:id])
-  	redirect_to show_user_path(id: params[:id])
+  	# redirect_to show_user_path(id: params[:id])
   end
 
   def unfollow
-  	current_user.following.delete(params[:id].to_i)
+  	current_user.following.delete('id' => params[:id])
   	current_user.save
 
   	redirect_to show_user_path(id: params[:id])
   end
+
+  def following
+    @user = User.find(params[:id])
+    @users = []
+
+    User.all.each do |user|
+      if @user.following.include?('id' => user.id.to_s)
+        @users.push(user)
+      end  
+    end  
+  end  
+
+  def followers
+    @user = User.find(params[:id])
+    @users = []
+
+    User.all.each do |user|
+      if user.following.include?('id' => @user.id.to_s)
+        @users.push(user)
+      end  
+    end
+  end  
+
 end
